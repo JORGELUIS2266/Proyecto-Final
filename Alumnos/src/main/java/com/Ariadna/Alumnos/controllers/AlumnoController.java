fb1855753fd8d254b8287f8a96c5a1aac8e87e3c
Ariadnamonserrat10 <aparicioariadna224@gmail.com>
2025-05-07 01:56:17 -0600

    Act5-Creando metodo POST para la insección de datos

--- Alumnos/src/main/java/com/Ariadna/Alumnos/controllers/AlumnoController.java
+++ Alumnos/src/main/java/com/Ariadna/Alumnos/controllers/AlumnoController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.Ariadna.Alumnos.model.Alumno;
 import com.Ariadna.Alumnos.repository.AlumnoRepository;
@@ -16,8 +18,16 @@ public class AlumnoController {
     @Autowired
     private AlumnoRepository alumnoRepository;
 
+    // Metodo get para traer todos los alumnos 
     @GetMapping("/traer-alumnos")
     public List<Alumno> TraerAlumnos() {
         return alumnoRepository.findAll();
     }
+
+    // Metodo para insertar un alumno
+    @PostMapping("/insertar-alumno")
+    public Alumno insertarAlumno(@RequestBody Alumno alumno) {
+        return alumnoRepository.save(alumno);
+    }
+
 }
