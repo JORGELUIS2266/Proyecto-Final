3167378da2cba6e5cdb7d5bc5c15466af7780d65
Ariadnamonserrat10 <aparicioariadna224@gmail.com>
2025-05-07 02:13:33 -0600

    Act7-Creación del metodo Eliminar

--- Alumnos/src/main/java/com/Ariadna/Alumnos/controllers/AlumnoController.java
+++ Alumnos/src/main/java/com/Ariadna/Alumnos/controllers/AlumnoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -50,6 +51,12 @@ public class AlumnoController {
             Alumno actualizado = alumnoRepository.save(alumnoExistente);
             return ResponseEntity.ok(actualizado);
         }).orElse(ResponseEntity.notFound().build());
+    }
+
+    // Metodo para eliminar un alumno
+    @DeleteMapping("/eliminar-alumno/{id}")
+    public void eliminarAlumno(@PathVariable Long id) {
+        alumnoRepository.deleteById(id);
     }
 
 }
