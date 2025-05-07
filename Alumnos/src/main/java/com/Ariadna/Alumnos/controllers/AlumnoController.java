ddd3de58ee0e004ff60ac3345b2f6dec7afb9dcb
Ariadnamonserrat10 <aparicioariadna224@gmail.com>
2025-05-07 02:06:37 -0600

    Act6-Creando metodo PUT para editar un dato

--- Alumnos/src/main/java/com/Ariadna/Alumnos/controllers/AlumnoController.java
+++ Alumnos/src/main/java/com/Ariadna/Alumnos/controllers/AlumnoController.java
@@ -3,11 +3,14 @@ package com.Ariadna.Alumnos.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.Ariadna.Alumnos.model.Alumno;
 import com.Ariadna.Alumnos.repository.AlumnoRepository;
@@ -28,6 +31,25 @@ public class AlumnoController {
     @PostMapping("/insertar-alumno")
     public Alumno insertarAlumno(@RequestBody Alumno alumno) {
         return alumnoRepository.save(alumno);
+    }
+
+    //Metodo para editar un alumno
+    @PutMapping("/editar-alumno/{id}")
+    public ResponseEntity<Alumno> ActualizarAlumno(@PathVariable Long id, @RequestBody Alumno alumno) {
+        return alumnoRepository.findById(id).map(alumnoExistente -> {
+            alumnoExistente.setNumeroControl(alumno.getNumeroControl());
+            alumnoExistente.setNombre(alumno.getNombre());
+            alumnoExistente.setApellidos(alumno.getApellidos());
+            alumnoExistente.setCorreo(alumno.getCorreo());
+            alumnoExistente.setTelefono(alumno.getTelefono());
+            alumnoExistente.setCarrera(alumno.getCarrera());
+            alumnoExistente.setSemestre(alumno.getSemestre());
+            alumnoExistente.setGrupo(alumno.getGrupo());
+            alumnoExistente.setImagenURL(alumno.getImagenURL());
+
+            Alumno actualizado = alumnoRepository.save(alumnoExistente);
+            return ResponseEntity.ok(actualizado);
+        }).orElse(ResponseEntity.notFound().build());
     }
 
 }
