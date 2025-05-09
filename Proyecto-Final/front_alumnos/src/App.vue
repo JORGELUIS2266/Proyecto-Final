1abf927b8df03de68343daedce57479e3f536339
roldanUriel <uriroldan06@gmail.com>
2025-05-09 00:37:00 -0600

    Act-14 Dando permisos de CrossOrigin

--- Proyecto-Final/front_alumnos/src/App.vue
+++ Proyecto-Final/front_alumnos/src/App.vue
@@ -2,6 +2,13 @@
 import { ref,onMounted } from 'vue';
 import axios from 'axios';
 const alumnos = ref([]);
+const nuevoAlumno = ref({
+  nombre: '',
+  apellidos: '',
+  carrera: '',
+  telefono: '',
+  imagenURL: ''
+});
 const cargarAlumnos = async () => {
   const response = await axios.get('http://localhost:8081/alumnos/traer-alumnos')
   alumnos.value = response.data;
@@ -13,7 +20,87 @@ onMounted( cargarAlumnos);
 
 <template>
 <div class="container">
-  <button class="btn btn-primary">Boton</button>
+  <div class="row">
+    <div class="col-md-12 mt-4">
+
+  <div class="card shadow p-4 mb-4">
+    <h2 class="text-center">Formulario de Alumnos</h2>
+    <form @submit.prevent="AgregarAlumno">
+      <div class="row">
+      <div class="col-md-6 mb-3">
+        <label for="nombre" class="form-label">Nombre</label>
+        <input type="text" class="form-control" id="nombre" v-model="nuevoAlumno.nombre" required>
+      </div>
+      <div class="col-md-6 mb-3">
+        <label for="apellidos" class="form-label">Apellidos</label>
+        <input type="text" class="form-control" id="apellidos" v-model="nuevoAlumno.apellidos" required>
+      </div>
+      <div class="col-md-6 mb-3">
+        <label for="carrera" class="form-label">Carrera</label>
+        <input type="text" class="form-control" id="carrera" v-model="nuevoAlumno.carrera" required>
+      </div>
+      <div class="col-md-6 mb-3">
+        <label for="telefono" class="form-label">Telefono</label>
+        <input type="text" class="form-control" id="telefono" v-model="nuevoAlumno.telefono" required>
+      </div>
+      <div class="col-md-6 mb-3">
+        <label for="imagenURL" class="form-label">Imagen URL</label>
+        <input type="text" class="form-control" id="imagenURL" v-model="nuevoAlumno.imagenURL">
+      </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Agregar Alumno</button>
+    </form>
+    
+     
+
+
+  </div>
+    </div>
+    
+    <div class="col-md-12">
+      <div class="card shadow">
+        <div class="card-body">
+          <h5 class="card-title mb-3">Tabla de Alumnos</h5>
+           <table class="table table-hover align-middle">
+  <thead class="table-light">
+           
+
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellidos</th>
+      <th scope="col">Carrera</th>
+      <th scope="col">telefono</th>
+      <th scope="col">imagen</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="alumno in alumnos" :key="alumno.id">
+      <td>{{ alumno.id }}</td>
+      <td>{{ alumno.nombre }}</td>
+      <td>{{ alumno.apellidos }}</td>
+      <td>{{ alumno.carrera }}</td>
+      <td>{{ alumno.telefono }}</td>
+      <td><img :src="alumno.imagenURL" alt="Imagen de Alumno" width="50"></td>
+      <td>
+        
+        <button class="btn btn-danger">Eliminar</button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+      </div>
+     
+     
+
+    </div>
+  </div>
+  
+    
+
+
 </div>
 </template>
 
