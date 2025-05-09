7636849f7a465a98e55ccbdb71e5d1c7ec7a6fa1
roldanUriel <uriroldan06@gmail.com>
2025-05-08 23:12:09 -0600

    Act13-Creando metodo GET para traer la informacion de los alumnos

--- Proyecto-Final/front_alumnos/src/App.vue
+++ Proyecto-Final/front_alumnos/src/App.vue
@@ -1,14 +1,20 @@
 <script setup>
+import { ref,onMounted } from 'vue';
+import axios from 'axios';
+const alumnos = ref([]);
+const cargarAlumnos = async () => {
+  const response = await axios.get('http://localhost:8081/alumnos/traer-alumnos')
+  alumnos.value = response.data;
+  console.log(alumnos.value);
 
+}
+onMounted( cargarAlumnos);
 </script>
 
 <template>
-  <div class="container">
-    <button class="btn btn-primary">boton</button>
-    </div>
-    
- 
-  
+<div class="container">
+  <button class="btn btn-primary">Boton</button>
+</div>
 </template>
 
 <style scoped>
